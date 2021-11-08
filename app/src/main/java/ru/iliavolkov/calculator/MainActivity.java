4dbb5331f1b66e2d9e51ddcba87b961016057082
Илья <ilia.volkov2016@mail.ru>
2021-11-08 18:03:34 +0300

    1

--- app/src/main/java/ru/iliavolkov/calculator/MainActivity.java
+++ app/src/main/java/ru/iliavolkov/calculator/MainActivity.java
@@ -1,18 +1,18 @@
 package ru.iliavolkov.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Arrays;
-
 public class MainActivity extends AppCompatActivity {
 
-    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,divide,multiply,fold,subtract;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,divide,multiply,fold,subtract, deleteOneElement,deleteAll;
     TextView field1, field2, field3;
+    double number1 = 0;
+    double number2 = 0;
+    double result = 0;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -37,31 +37,60 @@ public class MainActivity extends AppCompatActivity {
         field1 = findViewById(R.id.field1);
         field2 = findViewById(R.id.field2);
         field3 = findViewById(R.id.field3);
+        deleteOneElement = findViewById(R.id.delete_one_element);
+        deleteAll = findViewById(R.id.delete_text);
 
-//        btn1.setOnClickListener(v -> setNumber(1));
-//        btn2.setOnClickListener(v -> setNumber(2));
-//        btn3.setOnClickListener(v -> setNumber(3));
-//        btn4.setOnClickListener(v -> setNumber(4));
-//        btn5.setOnClickListener(v -> setNumber(5));
-//        btn6.setOnClickListener(v -> setNumber(6));
-//        btn7.setOnClickListener(v -> setNumber(7));
-//        btn8.setOnClickListener(v -> setNumber(8));
-//        btn9.setOnClickListener(v -> setNumber(9));
-//        btn0.setOnClickListener(v -> setNumber(0));
-//        divide.setOnClickListener(v -> operation("/"));
-//        multiply.setOnClickListener(v -> operation("*"));
-//        fold.setOnClickListener(v -> operation("+"));
-//        subtract.setOnClickListener(v -> operation("-"));
-
-
+        btn1.setOnClickListener(v -> setNumber(9));
+        btn2.setOnClickListener(v -> setNumber(9));
+        btn3.setOnClickListener(v -> setNumber(9));
+        btn4.setOnClickListener(v -> setNumber(9));
+        btn5.setOnClickListener(v -> setNumber(9));
+        btn6.setOnClickListener(v -> setNumber(9));
+        btn7.setOnClickListener(v -> setNumber(9));
+        btn8.setOnClickListener(v -> setNumber(9));
+        btn9.setOnClickListener(v -> setNumber(9));
+        btn0.setOnClickListener(v -> setNumber(9));
+        divide.setOnClickListener(v -> operation("/"));
+        multiply.setOnClickListener(v -> operation("*"));
+        fold.setOnClickListener(v -> operation("+"));
+        subtract.setOnClickListener(v -> operation("-"));
+        deleteAll.setOnClickListener(v -> {
+            field1.setText("");
+            field2.setText("");
+            field3.setText("");
+            number1 = 0;
+            number2 = 0;
+            result = 0;
+        });
+        deleteOneElement.setOnClickListener(v -> field1.setText("1000000000"));
+//        deleteOneItem()
     }
 
-//    private void operation(String s) {
-//        double number1 = 0;
-//        double number2 = 0;
-//        double result = 0;
-//        try { number1 = Double.parseDouble(getField(field2.getText().toString())); } catch (Exception e) {}
-//        try { number2 = Double.parseDouble(field1.getText().toString()); } catch (Exception e) {}
+    private void deleteOneItem() {
+        String[] arr = field1.getText().toString().split("");
+        String resultStr = "";
+        for (int i = 0; i < arr.length-1; i++){
+            resultStr += arr[i];
+        }
+        field1.setText(resultStr);
+    }
+
+    private void operation(String s) {
+        if (field2.getText().toString().equals("")) {
+            if (field1.length() > 0) {
+                field2.setText(field1.getText() + " " + s);
+                field1.setText(null);
+            }
+        } else {
+            if (field1.length() > 0) {
+                try { number1 = Double.parseDouble(getField(field2.getText().toString())); } catch (Exception e) {}
+                try { number2 = Double.parseDouble(field1.getText().toString()); } catch (Exception e) {}
+                result = number1 + number2;
+                field1.setText((int) result);
+                field3.setText(String.valueOf(result));
+            }
+        }
+
 //        try {
 //            String[] strings = field2.getText().toString().split("");
 //            switch (strings[strings.length]) {
@@ -91,19 +120,19 @@ public class MainActivity extends AppCompatActivity {
 //                field2.setText(string);
 //            }
 //        }
-//    }
-//
-//    private String getField(String field) {
-//        String[] arr = field.split("[+-/* ]");
-//        String string = "";
-//        for (int i = 0; i < arr.length; i++) string += arr[i];
-//        return string;
-//    }
-//
-//    @SuppressLint("SetTextI18n")
-//    private void setNumber(int i) {
-//        if (field1.length() <= 12) {
-//            field1.setText(field1.getText() + "" + i);
-//        }
-//    }
+    }
+
+    private String getField(String field) {
+        String[] arr = field.split("[+-/* ]");
+        String string = "";
+        for (int i = 0; i < arr.length; i++) string += arr[i];
+        return string;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void setNumber(int i) {
+        if (field1.length() <= 12) {
+            field1.setText(field1.getText() + "" + i);
+        }
+    }
 }
