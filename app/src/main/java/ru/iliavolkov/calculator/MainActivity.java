db3046ce6cef9daf0d82b37e9904ed6b4b464845
Илья <ilia.volkov2016@mail.ru>
2021-11-17 00:45:28 +0300

    Начинаем делать %

--- app/src/main/java/ru/iliavolkov/calculator/MainActivity.java
+++ app/src/main/java/ru/iliavolkov/calculator/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void deleteTopBar() {
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window w = getWindow();
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
